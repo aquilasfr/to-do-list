05843d91566ae47567ae7ead96b29952feeb7017
Áquilas <aquilasfr>
2021-04-07 18:31:42 -0300

    create tags ul and li in js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,35 @@
 var inputTask= document.querySelector('.form-task__input');
 var formTask= document.querySelector('.form-task');
+var toDoList= document.querySelector('.to-do__list')
+
 
 function handleFormTask(event){
     event.preventDefault()
-    console.log(inputTask.value);
+    createToDoItem()
+    formTask.reset()
+    
+}
+
+function createToDoItem() {
+    var checkbox = document.createElement('input')
+    checkbox.setAttribute("type", "checkbox")
+
+    var label = document.createElement('label')
+    label.textContent = inputTask.value
+    
+    var listItem = document.createElement('li'); 
+    listItem.classList.add("to-do__task")   
+   
+    listItem.appendChild(checkbox)
+    listItem.appendChild(label)
+    toDoList.appendChild(listItem)
+
 }
 
 formTask.addEventListener('submit', handleFormTask);
+
+
+
+
 
 
